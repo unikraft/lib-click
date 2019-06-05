e726cf3868ff9eba5913801f738504f8575aa1cd
Florian Schmidt <florian.schmidt@neclab.eu>
2019-06-05 14:43:26 +0200

    Initial public release: stubs.cc for unfinished glue implementations
    Signed-off-by: Florian Schmidt <florian.schmidt@neclab.eu>
    Reviewed-by: Felipe Huici <felipe.huici@neclab.eu>

---
+++ stubs.cc
@@ -0,0 +1,36 @@
+#include <errno.h>
+#include <click/config.h>
+#include <click/error.hh>
+#include <click/string.hh>
+
+/* Unikraft does not support files yet, this function is to
+   be able to read Click configs from files. */
+String file_string(String s __unused, ErrorHandler *errh)
+{
+	errh->error("no support for files yet!");
+	return String();
+}
+
+String clickpath_find_file(const String &filename __unused,
+		const char *subdir __unused, String default_path __unused,
+		ErrorHandler *errh)
+{
+	errh->error("no support for finding files!");
+	return String();
+}
+
+void click_signal(int signum __unused, void (*handler)(int) __unused,
+		bool resethand __unused)
+{
+}
+
+extern "C" {
+int dup(int oldfd); /* we don't have a header file, but the symbol is there */
+
+int pipe(int pipefd[2])
+{
+	pipefd[0] = dup(0);
+	pipefd[1] = dup(1);
+	return 0;
+}
+} /* extern "C" */
